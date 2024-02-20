08b555d8858640db7416511be85fa77229c5e4f2
Md. Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-02-20 15:06:37 +0600

    tofsil join query added

---
+++ solution/dc/query/dc_sub_queries/tofsil.sql
@@ -0,0 +1,17 @@
+select 
+tfsl.application_id,
+GROUP_CONCAT(`sensers_talika`)sensers_talika,
+GROUP_CONCAT(`sensers_talika_cromik_no`)sensers_talika_cromik_no,
+GROUP_CONCAT(`khatian_type`)khatian_type,
+GROUP_CONCAT(`khatian_no`)khatian_no,
+GROUP_CONCAT(`dag_no`) dag_no,
+GROUP_CONCAT(`land_amount`)land_amount,
+GROUP_CONCAT(`lijcreto_land_amount`)lijcreto_land_amount,
+GROUP_CONCAT(`mot_dabir_poriman`)mot_dabir_poriman,
+GROUP_CONCAT(`mot_dabir_poriman_value` )mot_dabir_poriman_value
+from(SELECT ti.application_id,ti.`sensers_talika`,ti.`sensers_talika_cromik_no`,
+tki.`khatian_type`,tki.`khatian_no`,tki.`dag_no`,tki.`land_amount`,tki.`lijcreto_land_amount`,
+tki.`mot_dabir_poriman`,tki.`mot_dabir_poriman_value`
+FROM tofsil_info ti
+LEFT JOIN tofsil_khatian_info tki ON tki.tofsil_info_id = ti.id)tfsl
+group by tfsl.application_id
