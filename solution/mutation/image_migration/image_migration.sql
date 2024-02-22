60e41f55caf67c4efbc457d613b0239ccbad018c
Md. Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-02-22 10:35:09 +0600

    image_migration query

---
+++ solution/mutation/image_migration/image_migration.sql
@@ -0,0 +1,15 @@
+SELECT * 
+FROM (SELECT
+    id,
+	CONVERT(signature USING utf8) AS image_path,
+    TO_BASE64(signature) AS signature,
+    signature_date,
+    YEAR(signature_date) AS YEAR,
+    MONTH(signature_date) AS MONTH
+FROM `mutation_khulna`.`khotian_comments`
+WHERE id > "${max_ref_id}" 
+AND signature IS NOT NULL 
+AND signature!='' 
+ORDER BY id LIMIT 10) tmp 
+WHERE signature!=''
+ORDER BY id LIMIT 5;
