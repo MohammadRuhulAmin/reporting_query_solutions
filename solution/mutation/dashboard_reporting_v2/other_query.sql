73d0c7990ccc9cf0e9cacc3d8f51dac403a9445c
Md. Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-02-22 10:02:36 +0600

    dashboard_reporting_v2 union office_id update query

--- solution/mutation/dashboard_reporting_v2/other_query.sql
+++ solution/mutation/dashboard_reporting_v2/other_query.sql
@@ -1,4 +1,15 @@
 SELECT id upazila_id  
 FROM `mutation_chottogram`.`dglr_upazilas` 
 WHERE `division_id`=2 AND upazila_id>0
-order by id
+order by id;
+
+
+SELECT `union_office_id` 
+FROM `mutation_chottogram`.`office_wise_mouja_assign` 
+WHERE `mouja_id`=${mouja_id};
+
+
+
+ UPDATE `mutation_chottogram`.`dashboard_reporting_raw` 
+ SET `union_office_id`=${union_office_id} 
+ WHERE `mouja_id`=${mouja_id} AND `union_office_id` IS NULL;
