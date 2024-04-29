f5382f3484330a3a2673e0a493b808af2a7b9c18
Md. Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-04-29 13:09:03 +0600

    updated on users marge query

--- solution/sdg/query/user_mapping/user_temp_mapping.sql
+++ solution/sdg/query/user_mapping/user_temp_mapping.sql
@@ -1,10 +1,10 @@
-SELECT temp_marge.provider_id,temp_marge.status,temp_marge.id,temp_marge.user_id,temp_marge.created_by,
+SELECT sil.serial_no,temp_marge.indicator_id,temp_marge.provider_id,temp_marge.status,temp_marge.id,temp_marge.user_id,temp_marge.created_by,
 temp_marge.`office_ministry_id`,temp_marge.`office_layer_id`,
 temp_marge.`office_origin_id`,temp_marge.`office_id`,temp_spl.sdg_provider_id,temp_spl.username
 FROM 
-(SELECT temp_sidp.provider_id,temp_sidp.status,sp.id,sp.user_id,sp.created_by,
+(SELECT temp_sidp.provider_id,temp_sidp.indicator_id,temp_sidp.status,sp.id,sp.user_id,sp.created_by,
 sp.`office_ministry_id`,sp.`office_layer_id`,sp.`office_origin_id`,sp.`office_id`
-FROM(SELECT DISTINCT sidp.`provider_id`,sidp.`status` 
+FROM(SELECT DISTINCT sidp.`provider_id`,sidp.`status`,sidp.indicator_id 
 FROM `sdg_v1_v2_live`.`sdg_indic_def_providers` sidp
 WHERE sidp.`status` = 3 ORDER BY sidp.provider_id)temp_sidp
 LEFT JOIN `sdg_v1_v2_live`.`sdg_providers` sp ON sp.id = temp_sidp.provider_id)temp_marge
@@ -13,4 +13,6 @@ FROM(SELECT  spl.sdg_provider_id,spl.username
 FROM `sdg_v1_v2_live`.`sdg_provider_logs` spl 
 GROUP BY spl.sdg_provider_id,spl.username
 ORDER BY spl.sdg_provider_id,username DESC)tmp
-GROUP BY tmp.sdg_provider_id)temp_spl ON temp_spl.sdg_provider_id = temp_marge.provider_id;
+GROUP BY tmp.sdg_provider_id)temp_spl ON temp_spl.sdg_provider_id = temp_marge.provider_id
+LEFT JOIN `sdg_v1_v2_live`.`sdg_indicator_langs` sil ON sil.id = temp_marge.indicator_id AND sil.language_id = 1
+ORDER BY sil.serial_no;
