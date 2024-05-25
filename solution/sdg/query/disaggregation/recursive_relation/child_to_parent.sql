d489bb5ac9caf89796024961d26d13f347098b6c
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-05-25 14:29:30 +0600

    adding level or serial in child to parent query

--- solution/sdg/query/disaggregation/recursive_relation/child_to_parent.sql
+++ solution/sdg/query/disaggregation/recursive_relation/child_to_parent.sql
@@ -12,3 +12,21 @@ WITH RECURSIVE Ancestors AS (
     INNER JOIN Ancestors a ON s.disaggregation_id = a.parent_id
 )
 SELECT * FROM Ancestors;
+
+
+# Adding Level 
+WITH RECURSIVE Ancestors AS (
+    -- Anchor member: Select the initial row that starts the recursion (the given disaggregation_id)
+    SELECT disaggregation_id, parent_id, NAME, 1 AS LEVEL
+    FROM sdg_disaggregation_langs
+    WHERE disaggregation_id  = 99
+
+    UNION ALL
+
+    -- Recursive member: Select rows that are parents of the previously selected rows
+    SELECT s.disaggregation_id, s.parent_id, s.name, a.level + 1 AS LEVEL
+    FROM sdg_disaggregation_langs s
+    INNER JOIN Ancestors a ON s.disaggregation_id = a.parent_id
+)
+SELECT ROW_NUMBER() OVER (ORDER BY LEVEL) AS LEVEL, disaggregation_id, parent_id, NAME
+FROM Ancestors;
