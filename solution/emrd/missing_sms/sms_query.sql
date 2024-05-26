f89fe1f9969c02249c9cc19f6ae36844b1f027f1
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-05-26 17:13:36 +0600

    Summary query for type 4 company

--- solution/emrd/missing_sms/sms_query.sql
+++ solution/emrd/missing_sms/sms_query.sql
@@ -4,3 +4,17 @@ LEFT JOIN emrd_uat.users u ON u.org_id = gp.org_id
 WHERE  oi.org_short_name = "BAPEX"
 AND DATE(gp.created_at) =  DATE_SUB(CURDATE(),INTERVAL 1 DAY)
 AND u.mobile IS NOT NULL;
+
+
+# Summary result FOR Type:4 
+SELECT 
+  SUM(CASE WHEN temp.sector = 'Power' THEN temp.total_supply ELSE 0 END) AS `Power`,
+  SUM(CASE WHEN temp.sector = 'Fertilizer' THEN temp.total_supply ELSE 0 END) AS Fertilizer,
+  SUM(CASE WHEN temp.sector = 'Other' THEN temp.total_supply ELSE 0 END) AS Other
+FROM (
+  SELECT gds.sector, SUM(gd.supply) AS total_supply 
+  FROM `gas_distribution` gd
+  LEFT JOIN `gas_distribution_sector` gds ON gds.id = gd.sector_id
+  WHERE gd.org_id = 14
+  GROUP BY gds.sector
+) temp;
