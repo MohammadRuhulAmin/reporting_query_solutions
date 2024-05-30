a2763a42bb2cb65a126539da2a8d842e7f673aa0
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-05-30 11:16:50 +0600

    level wise child parent relation showing

---
+++ solution/sdg/query/source_langs/child_parent_relation.sql
@@ -0,0 +1,13 @@
+WITH RECURSIVE ancestors AS (
+  SELECT source_id, parent_id, NAME, 1 AS LEVEL
+  FROM sdg_source_langs 
+  WHERE source_id = 179 AND language_id = 1
+  
+  UNION ALL
+ 
+  SELECT s.source_id, s.parent_id, s.name, a.level + 1 AS LEVEL
+  FROM sdg_source_langs s
+  INNER JOIN ancestors a ON s.source_id = a.parent_id AND s.language_id = 1
+)
+SELECT ROW_NUMBER() OVER (ORDER BY LEVEL) AS LEVEL, source_id, parent_id, NAME
+FROM ancestors
