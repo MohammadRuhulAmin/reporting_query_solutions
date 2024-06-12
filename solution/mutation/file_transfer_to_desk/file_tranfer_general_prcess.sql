4fdde5ae8c7bd5cb414b79ef0a48a1aa41ea119b
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-06-12 16:55:23 +0600

    query added using limit and offset

--- solution/mutation/file_transfer_to_desk/file_tranfer_general_prcess.sql
+++ solution/mutation/file_transfer_to_desk/file_tranfer_general_prcess.sql
@@ -1,3 +1,27 @@
+#using Limit Offset
+
+SELECT temp.draft_mig_date
+FROM (
+SELECT CURDATE()-1 - INTERVAL daynum DAY AS draft_mig_date
+FROM
+(
+    SELECT sl_first* 10+ sl_second daynum
+    FROM
+        (SELECT 0 sl_first UNION SELECT 1 UNION SELECT 2 UNION SELECT 3) temp_x,
+        (SELECT 0 sl_second UNION SELECT 1 UNION SELECT 2 UNION SELECT 3
+        UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7
+        UNION SELECT 8 UNION SELECT 9) temp_y
+    WHERE sl_first*10+sl_second <= 15
+) temp_z
+ORDER BY draft_mig_date DESC)temp 
+WHERE 
+temp.draft_mig_date NOT IN (SELECT holiday_date FROM govt_holiday)
+LIMIT 1 OFFSET 4;
+
+
+
+#using ROW_NUMBER() OVER() Function 
+
 SELECT tempx.* FROM (
 SELECT temp.today_date, ROW_NUMBER() OVER (ORDER BY temp.today_date DESC) AS working_day_sl 
 FROM (
