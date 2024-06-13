c763326209956a08059c99a182d6e7de11338e3a
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-06-13 10:44:17 +0600

    user information collection, duplicate entry removed

--- solution/emrd/daily_summary_sms/type_wise_sms/type_6_hcu/collect_user.sql
+++ solution/emrd/daily_summary_sms/type_wise_sms/type_6_hcu/collect_user.sql
@@ -1,4 +1,5 @@
-SELECT email,id,mobile,TRIM(CONCAT(" ", IFNULL(first_name,""), " ", IFNULL(last_name," "))) AS fullName FROM users WHERE org_id = 5 AND mobile IS NOT NULL
+SELECT temx.* FROM 
+(SELECT email,id,mobile,TRIM(CONCAT(" ", IFNULL(first_name,""), " ", IFNULL(last_name," "))) AS fullName FROM users WHERE org_id = 5 AND mobile IS NOT NULL
 UNION 
 SELECT email,id,mobile,TRIM(CONCAT(" ", IFNULL(first_name,""), " ", IFNULL(last_name," "))) AS fullName FROM users WHERE id IN(118,217,258,262)
 UNION 
@@ -7,4 +8,5 @@ SELECT DISTINCT u.email,u.id,u.mobile,TRIM(CONCAT(" ", IFNULL(u.first_name,""), 
 LEFT JOIN model_has_roles mhr ON mhr.role_id = r.id
 WHERE NAME LIKE "%Managing Director%"
 AND mhr.model_id IS NOT NULL)temp1
-LEFT JOIN users u ON u.id = temp1.user_id;
+LEFT JOIN users u ON u.id = temp1.user_id)temx
+GROUP BY temx.mobile, temx.id;
