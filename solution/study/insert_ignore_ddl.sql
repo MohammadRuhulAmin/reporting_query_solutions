0eecfc96e6f8e8a061fd6b2a774c5491c002e1e5
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-06-18 23:11:49 +0600

    insert ignore command

---
+++ solution/study/insert_ignore_ddl.sql
@@ -0,0 +1,13 @@
+#To ignore the same record while inserting using insert command
+#step1: first we have to specify the column name to be unique 
+#step2: then execute the insert ignore command 
+
+CREATE TABLE  temp (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    NAME VARCHAR(255),
+    email VARCHAR(255),
+    UNIQUE (NAME, email)
+);
+
+INSERT IGNORE INTO temp(NAME,email)
+VALUES('ruhul','r@gmail.com'),('ruhul','r@gmail.com'),('ruhul','r@gmail.com');
