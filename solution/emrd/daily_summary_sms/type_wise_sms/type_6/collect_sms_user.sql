5081f683c942b86e57ddd2cdb81ba12a5cd2c905
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-06-27 11:41:07 +0600

    collect sms user added admin users

--- solution/emrd/daily_summary_sms/type_wise_sms/type_6/collect_sms_user.sql
+++ solution/emrd/daily_summary_sms/type_wise_sms/type_6/collect_sms_user.sql
@@ -1,12 +1,30 @@
-SELECT temx.* FROM 
-(SELECT email,id,mobile,TRIM(CONCAT(" ", IFNULL(first_name,""), " ", IFNULL(last_name," "))) AS fullName FROM users WHERE org_id = 5 AND mobile IS NOT NULL
+
+SELECT DISTINCT tempx.mobile, tempx.id, tempx.mobile FROM 
+(SELECT email,id,mobile FROM users WHERE org_id = 5 AND mobile IS NOT NULL
+
 UNION 
-SELECT email,id,mobile,TRIM(CONCAT(" ", IFNULL(first_name,""), " ", IFNULL(last_name," "))) AS fullName FROM users WHERE id IN(118,217,258,262)
+
+#getting all the System author
+SELECT email,id,mobile FROM users WHERE id IN(118,217,258,262)
+
 UNION 
-SELECT DISTINCT u.email,u.id,u.mobile,TRIM(CONCAT(" ", IFNULL(u.first_name,""), " ", IFNULL(u.last_name," "))) AS fullName
+
+#getting the managing directors
+SELECT DISTINCT u.email,u.id,u.mobile
  FROM(SELECT DISTINCT mhr.model_id AS user_id FROM roles r
 LEFT JOIN model_has_roles mhr ON mhr.role_id = r.id
 WHERE NAME LIKE "%Managing Director%"
 AND mhr.model_id IS NOT NULL)temp1
-LEFT JOIN users u ON u.id = temp1.user_id)temx
-GROUP BY temx.mobile, temx.id;
+LEFT JOIN users u ON u.id = temp1.user_id
+
+UNION
+
+# getting all the admins
+SELECT u.email,u.id,u.mobile FROM(SELECT * FROM roles r
+LEFT JOIN model_has_roles mhr ON mhr.role_id = r.id
+WHERE r.name LIKE "%Admin") temp2
+LEFT JOIN users u ON u.id = temp2.model_id WHERE u.mobile IS NOT NULL
+
+)tempx
+GROUP BY tempx.mobile, tempx.id
+ORDER BY tempx.id;
