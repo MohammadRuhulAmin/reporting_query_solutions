4f3adc541fc3d080793a269db82f3d6d139f8b01
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-06-27 12:08:53 +0600

    RPGCL org_id = 19 added in hcu main sms query

--- solution/emrd/daily_summary_sms/type_wise_sms/type_6/hcu_main_query.sql
+++ solution/emrd/daily_summary_sms/type_wise_sms/type_6/hcu_main_query.sql
@@ -9,9 +9,9 @@ WHERE report_date = DATE_SUB(CURDATE(),  INTERVAL 1 DAY) AND production_type = "
 AND report_date = DATE_SUB(CURDATE(), INTERVAL 1 DAY)) AS lng_gas_production,
 (SELECT 
 CONCAT(
-FLOOR(IFNULL(SUM(CASE WHEN gas_cat = "NOC" AND production_type = "gas" THEN production ELSE 0 END),0))," + ",
-FLOOR(IFNULL(SUM(CASE WHEN gas_cat = "IOC" AND production_type = "gas" THEN production ELSE 0 END),0))," + ",
-FLOOR(IFNULL(SUM(CASE WHEN gas_cat = "LNG" AND production_type = "gas" THEN production ELSE 0 END),0)) 
+FLOOR(IFNULL(SUM(CASE WHEN gas_cat = "NOC" THEN production ELSE 0 END),0))," + ",
+FLOOR(IFNULL(SUM(CASE WHEN gas_cat = "IOC" THEN production ELSE 0 END),0))," + ",
+FLOOR(IFNULL(SUM(CASE WHEN gas_cat = "LNG" THEN production ELSE 0 END),0)) 
 ) AS total_sep_sum
 FROM gas_production
 WHERE report_date = DATE_SUB(CURDATE(),INTERVAL 1 DAY))AS "NOC_IOC_LNG",
@@ -21,6 +21,13 @@ LEFT JOIN gas_production gp ON gp.org_id = oi.id
 AND gp.report_date = DATE_SUB(CURDATE(), INTERVAL 1 DAY)
 WHERE oi.id IN (1,9,10,11)
 UNION 
+
+SELECT DISTINCT oi.id, oi.org_short_name,gp.org_id FROM organization_info oi
+LEFT JOIN gas_production gp ON gp.org_id = oi.id AND (gas_cat = "LNG"
+AND gp.report_date = DATE_SUB(CURDATE(), INTERVAL 1 DAY)) 
+WHERE oi.id  = 19
+
+UNION
 SELECT oi.id, oi.org_short_name,cdp.org_id FROM organization_info oi 
 LEFT JOIN coal_daily_production cdp ON cdp.org_id = oi.id 
 AND cdp.report_date =  DATE_SUB(CURDATE(), INTERVAL 1 DAY) 
@@ -66,7 +73,6 @@ WHEN DAY(date_field) IN (1, 21, 31) THEN 'st' WHEN DAY(date_field) IN (2, 22) TH
 WHEN DAY(date_field) IN (3, 23) THEN 'rd' ELSE 'th'
 END,' ', DATE_FORMAT(date_field, '%M, %Y')) AS formatted_date
 FROM (SELECT DATE_SUB(CURDATE(), INTERVAL 1 DAY) AS date_field) AS temp_date) yesterday_date,
-(SELECT CONCAT(ROUND(SUM(production)),' MT', ' (', (DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 1 MONTH),'%M')) ,')') FROM lease_monthly_production ) AS hard_rock_production
-
+(SELECT CONCAT(ROUND(SUM(production)),' MT ', ' (', (DATE_FORMAT(NOW(),'%M')) ,')') FROM lease_monthly_production ) AS hard_rock_production
 
 
