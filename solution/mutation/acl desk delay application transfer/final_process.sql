cce7af0f0bded2aee965b5394fa8bab82327e1a1
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-06-29 14:31:35 +0600

    database added in step 2

--- solution/mutation/acl desk delay application transfer/final_process.sql
+++ solution/mutation/acl desk delay application transfer/final_process.sql
@@ -15,8 +15,9 @@ UNION SELECT 8 UNION SELECT 9) temp_y
 WHERE sl_first*10+sl_second <= 15) temp_z
 ORDER BY draft_mig_date DESC)temp 
 WHERE temp.draft_mig_date 
-NOT IN (SELECT holiday_date FROM govt_holiday)
-LIMIT 1 OFFSET {$nth_working_date};
+NOT IN (SELECT holiday_date FROM `mutation_ext`.govt_holiday)
+LIMIT 1 OFFSET ${nth_working_date};
+
 
 #step 3:
 #step2:(Get the application id)
