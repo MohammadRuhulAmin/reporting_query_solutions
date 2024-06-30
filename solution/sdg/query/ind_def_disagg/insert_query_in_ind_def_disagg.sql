aa8e7ca999231e155b0f213c5a89551fd0f55691
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-06-30 17:32:02 +0600

    calculation update for gas production

--- solution/sdg/query/ind_def_disagg/insert_query_in_ind_def_disagg.sql
+++ solution/sdg/query/ind_def_disagg/insert_query_in_ind_def_disagg.sql
@@ -6,6 +6,7 @@ LEFT JOIN indicator_data ind_data ON ind_data.id = idd.ind_data_id
 left join disaggregation_name dn on dn.id = idd.disagg_id
 left join ind_definitions ixd on ixd.ind_id = ind_data.ind_id
 where ind_data_id in(SELECT temp.ind_data_id FROM(SELECT ind_id,
+
 GROUP_CONCAT(DISTINCT id) ind_data_id FROM indicator_data
 GROUP BY ind_id)temp)
 order by ind_data.ind_id;
