ed9df2af86ab60780ad1ce4b8fae2f3a765c1e20
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-07-05 18:46:21 +0600

    repeat rows finding example

--- solution/research/find_repeated_rows.sql
+++ solution/research/find_repeated_rows.sql
@@ -5,3 +5,13 @@ WHERE (ind_data_id, disagg_id) IN (
     GROUP BY ind_data_id, disagg_id
     HAVING COUNT(*) > 1
 );
+
+SELECT type_id, NAME
+FROM disaggregation_name
+WHERE (type_id, NAME) IN (
+    SELECT type_id, NAME
+    FROM disaggregation_name
+    GROUP BY type_id, NAME
+    HAVING COUNT(*) > 1
+)
+ORDER BY type_id, NAME;
