746a9e844498abf48625eeb54d8034d2ec6a1652
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-07-07 14:45:58 +0600

    report those who provides data in gas_production table

--- solution/emrd/dashboard/common_report/company_wise_report.sql
+++ solution/emrd/dashboard/common_report/company_wise_report.sql
@@ -1,33 +1,26 @@
-SELECT oi.org_short_name,gp.org_id,MAX(gp.report_date) last_update
+# report those who provides data in gas_production table
+SELECT report_info.org_short_name company,report_info.last_update,report_info.report_submit_time,
+report_info.update_on_time,org_info.responsible,org_info.mobile
+FROM
+(SELECT oi.org_short_name,gp.org_id,gp.report_date last_update, gp.created_at report_submit_time,
+CASE WHEN (gp.created_at - INTERVAL 1 DAY >  CONCAT(gp.report_date," 17:00:00")) THEN "No"
+ELSE "Yes" END AS update_on_time
 FROM gas_production gp
-LEFT JOIN organization_info oi ON oi.id = gp.org_id
-GROUP BY gp.org_id ORDER BY gp.org_id;
-
-
-
-SELECT report_info.org_short_name,report_info.last_update,user_info.name Responsible,
-user_info.mobile contact
-FROM(SELECT oi.id,oi.org_short_name,gp.org_id,MAX(gp.report_date) last_update
-FROM gas_production gp
-LEFT JOIN organization_info oi ON oi.id = gp.org_id
-GROUP BY gp.org_id ORDER BY gp.org_id)report_info
-LEFT JOIN 
-(SELECT r.id,r.name ,mhr.model_id,u.mobile,u.org_id,
-CONCAT(u.first_name," ",u.last_name) Responsible FROM `roles` r
-LEFT JOIN model_has_roles mhr ON mhr.role_id = r.id
-LEFT JOIN users u ON u.id = mhr.model_id
-WHERE r.name LIKE "%Admin%" 
-AND u.mobile IS NOT NULL
-GROUP BY u.org_id
-ORDER BY r.id)user_info ON user_info.org_id = report_info.id;
-
-
-SELECT gp.org_id,gp.report_date,created_at FROM
-gas_production gp
 INNER JOIN
 (SELECT org_id,MAX(report_date) report_date FROM `gas_production`
 GROUP BY org_id) max_info
 ON max_info.org_id = gp.org_id AND 
 max_info.report_date = gp.report_date
+LEFT JOIN organization_info oi ON oi.id = gp.org_id 
 GROUP BY gp.org_id
-ORDER BY created_at DESC
+ORDER BY gp.created_at DESC)report_info
+LEFT JOIN 
+(SELECT r.id,r.name ,mhr.model_id,u.mobile,u.org_id,
+CONCAT(u.first_name," ",u.last_name) responsible FROM `roles` r
+LEFT JOIN model_has_roles mhr ON mhr.role_id = r.id
+LEFT JOIN users u ON u.id = mhr.model_id
+WHERE r.name LIKE "%Admin%" 
+AND u.mobile IS NOT NULL
+GROUP BY u.org_id
+ORDER BY r.id)org_info
+ON org_info.org_id = report_info.org_id;
