ff36cc37f9d597508b43fd70bd1781a2596ac288
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-08-11 13:36:10 +0600

    data status and due_data_years colum added

--- solution/sdg/summary_query/data_source_rpt_details.sql
+++ solution/sdg/summary_query/data_source_rpt_details.sql
@@ -1,4 +1,4 @@
-SELECT tbl_agency.*,ids.ind_id FROM(SELECT id ind_src_id,
+SELECT tbl_agency.*,ids.ind_id,si.data_status,si.due_data_years FROM(SELECT id ind_src_id,
 TRIM(CASE WHEN office_agency_id >0 THEN SUBSTRING_INDEX(SUBSTRING_INDEX(NAME, ',', 2), ',', -1) 
 WHEN office_agency_id = 0 AND ministry_division_id>0 THEN SUBSTRING_INDEX(SUBSTRING_INDEX(NAME, ',', 3), ',', -1)
 WHEN office_agency_id = 0 AND ministry_division_id = 0  AND ministry_id>0 THEN SUBSTRING_INDEX(NAME, ',', -1)
@@ -6,4 +6,5 @@ END) AS agency FROM ind_sources
 GROUP BY agency
 HAVING agency IS NOT NULL)tbl_agency
 LEFT JOIN ind_def_sources ids ON ids.source_id = tbl_agency.ind_src_id
-WHERE ids.ind_id IS NOT NULL
+LEFT JOIN sdg_indicators si ON si.id = ids.ind_id
+WHERE ids.ind_id IS NOT NULL;
