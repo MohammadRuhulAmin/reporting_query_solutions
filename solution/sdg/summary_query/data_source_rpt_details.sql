d864d5cbfa24c4703e1d7311c7d2d1fb3a9e3140
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-08-11 13:47:20 +0600

    data goal_id and title added

--- solution/sdg/summary_query/data_source_rpt_details.sql
+++ solution/sdg/summary_query/data_source_rpt_details.sql
@@ -1,4 +1,4 @@
-SELECT tbl_agency.*,ids.ind_id,si.data_status,si.due_data_years FROM(SELECT id ind_src_id,
+SELECT tbl_agency.*,ids.ind_id,sg.title,si.goal_id,si.target_id,si.data_status,si.due_data_years FROM(SELECT id ind_src_id,
 TRIM(CASE WHEN office_agency_id >0 THEN SUBSTRING_INDEX(SUBSTRING_INDEX(NAME, ',', 2), ',', -1) 
 WHEN office_agency_id = 0 AND ministry_division_id>0 THEN SUBSTRING_INDEX(SUBSTRING_INDEX(NAME, ',', 3), ',', -1)
 WHEN office_agency_id = 0 AND ministry_division_id = 0  AND ministry_id>0 THEN SUBSTRING_INDEX(NAME, ',', -1)
@@ -7,4 +7,5 @@ GROUP BY agency
 HAVING agency IS NOT NULL)tbl_agency
 LEFT JOIN ind_def_sources ids ON ids.source_id = tbl_agency.ind_src_id
 LEFT JOIN sdg_indicators si ON si.id = ids.ind_id
+LEFT JOIN sdg_goals sg ON sg.id = si.goal_id
 WHERE ids.ind_id IS NOT NULL;
