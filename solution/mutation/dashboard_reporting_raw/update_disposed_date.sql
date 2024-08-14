f0487b91bc2bcb27a3bf4b344b1ca575aabcf93b
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-08-14 12:56:54 +0600

    child indicators max reporting year added

--- solution/mutation/dashboard_reporting_raw/update_disposed_date.sql
+++ solution/mutation/dashboard_reporting_raw/update_disposed_date.sql
@@ -1,5 +1,5 @@
 #process 1:
-SELECT application_id FROM `dashboard_reporting_raw` WHERE disposed_date IS NULL;
+SELECT application_id FROM mutation_barisal.`dashboard_reporting_raw` WHERE disposed_date IS NULL;
 
 
 #process 2:
@@ -8,10 +8,10 @@ app_hist.id hist_id,
 a.`case_main_status_id`, 
 CASE WHEN a.`case_main_status_id` IN (9,27,39) THEN cas.`created`
 WHEN a.`case_main_status_id` IN (20,22) THEN csu_kha_final.`created` END disposed_date
-FROM `applications` a 
-LEFT JOIN `case_status_updates` app_hist ON app_hist.`application_id`=a.id AND a.`case_main_status_id`=app_hist.case_status_id
-LEFT JOIN `case_status_updates` cas ON a.id=cas.application_id AND cas.case_status_id IN (9,27,39)
-LEFT JOIN `case_status_updates` csu_kha_final ON csu_kha_final.`application_id`=a.`id`AND csu_kha_final.`case_status_id`=20
-WHERE a.`jomi_division_id`= 3  
+FROM mutation_barisal.`applications` a 
+LEFT JOIN mutation_barisal.`case_status_updates` app_hist ON app_hist.`application_id`=a.id AND a.`case_main_status_id`=app_hist.case_status_id
+LEFT JOIN mutation_barisal.`case_status_updates` cas ON a.id=cas.application_id AND cas.case_status_id IN (9,27,39)
+LEFT JOIN mutation_barisal.`case_status_updates` csu_kha_final ON csu_kha_final.`application_id`=a.`id`AND csu_kha_final.`case_status_id`=20
+WHERE a.`jomi_division_id`= 1  
 AND a.id = ${application_id} 
-GROUP BY a.`id`;
+GROUP BY a.`id`;
--- solution/sdg/summary_query/rough/data_source_rpt_details.sql
+++ solution/sdg/summary_query/rough/data_source_rpt_details.sql
@@ -27,7 +27,13 @@ LEFT JOIN sdg_indicator_details sid ON sid.indicator_id = ids.ind_id AND sid.lan
 WHERE ids.ind_id IS NOT NULL AND si.parent_indicator_id = 0 AND si.is_child = 0 
 AND si.is_npt_thirty_nine = 0 AND si.is_sdg = 1  AND si.is_plus_one = 0 )temp1
 LEFT JOIN 
-(SELECT si.id sdg_indicators_idx,MAX(ind.data_period) last_reporting_year FROM sdg_indicators si
+(SELECT si.id sdg_indicators_idx,si.has_child,CASE WHEN si.has_child = 0  THEN MAX(ind.data_period) 
+WHEN si.has_child = 1 THEN (SELECT MAX(data_period) FROM indicator_data WHERE ind_id IN 
+(SELECT GROUP_CONCAT(parent.id)  parent_id FROM(SELECT id FROM sdg_indicators WHERE has_child = 1)child
+LEFT JOIN (SELECT id,parent_indicator_id FROM sdg_indicators)parent
+ON child.id = parent.parent_indicator_id
+ WHERE parent_indicator_id = si.id))
+END AS last_reporting_year FROM sdg_indicators si
 LEFT JOIN indicator_data ind ON ind.ind_id = si.id
 GROUP BY si.id)temp2 ON temp2.sdg_indicators_idx = temp1.sdg_indicators_id)temp3
 LEFT JOIN 
@@ -61,55 +67,28 @@ LEFT JOIN sdg_indicator_details sid ON sid.indicator_id = ids.ind_id AND sid.lan
 WHERE ids.ind_id IS NOT NULL AND si.parent_indicator_id = 0 AND si.is_child = 0 
 AND si.is_npt_thirty_nine = 0 AND si.is_sdg = 1  AND si.is_plus_one = 0 )temp1
 LEFT JOIN 
-(SELECT si.id sdg_indicators_idx,MAX(ind.data_period) last_reporting_year FROM sdg_indicators si
+(SELECT si.id sdg_indicators_idx,si.has_child,CASE WHEN si.has_child = 0  THEN MAX(ind.data_period) 
+WHEN si.has_child = 1 THEN (SELECT MAX(data_period) FROM indicator_data WHERE ind_id IN 
+(SELECT GROUP_CONCAT(parent.id)  parent_id FROM(SELECT id FROM sdg_indicators WHERE has_child = 1)child
+LEFT JOIN (SELECT id,parent_indicator_id FROM sdg_indicators)parent
+ON child.id = parent.parent_indicator_id
+ WHERE parent_indicator_id = si.id))
+END AS last_reporting_year FROM sdg_indicators si
 LEFT JOIN indicator_data ind ON ind.ind_id = si.id
 GROUP BY si.id)temp2 ON temp2.sdg_indicators_idx = temp1.sdg_indicators_id)temp3
-LEFT JOIN 
+LEFT JOIN
 (SELECT ind_id,base_data_period,data_frequency_year, 
 base_data_period +(data_frequency_year*CEIL((YEAR(NOW())-base_data_period)/data_frequency_year)) next_reporting_year
 FROM ind_definitions WHERE base_data_period <> 0000)temp4 ON temp4.ind_id = temp3.foreign_id
 WHERE temp3.agency = "Bangladesh Bureau of Statistics" GROUP BY temp3.indicator_number;
-##### agency mapping  ################
 
-SELECT ind_sources.id ind_src_id, office_agency_id,office_agencies.name FROM ind_sources 
-LEFT JOIN office_agencies ON office_agencies.id = ind_sources.office_agency_id 
-WHERE office_agency_id > 0 GROUP BY office_agency_id
-UNION
-SELECT ind_sources.id, ministry_division_id,ministry_divisions.name FROM ind_sources 
-LEFT JOIN ministry_divisions ON ministry_divisions.id = ind_sources.ministry_division_id 
-WHERE office_agency_id = 0 AND ministry_division_id > 0 GROUP BY ministry_division_id
-UNION
-SELECT ind_sources.id, ministry_id,ministries.name FROM ind_sources 
-LEFT JOIN ministries ON ministries.id = ind_sources.ministry_id 
-WHERE office_agency_id = 0 AND ministry_division_id = 0 AND ministry_id >0 GROUP BY ministry_id;
-
-
-############################
-
-SELECT temp0.* FROM(SELECT tbl_agency.* FROM(SELECT ind_sources.id ind_src_id, office_agency_id,office_agencies.name agency FROM ind_sources 
-LEFT JOIN office_agencies ON office_agencies.id = ind_sources.office_agency_id 
-WHERE ind_sources.office_agency_id > 0 
-UNION
-SELECT ind_sources.id, ministry_division_id,ministry_divisions.name FROM ind_sources 
-LEFT JOIN ministry_divisions ON ministry_divisions.id = ind_sources.ministry_division_id 
-WHERE office_agency_id = 0 AND ministry_division_id > 0 
-UNION
-SELECT ind_sources.id, ministry_id,ministries.name FROM ind_sources 
-LEFT JOIN ministries ON ministries.id = ind_sources.ministry_id 
-WHERE office_agency_id = 0 AND ministry_division_id = 0 AND ministry_id >0
-UNION
-SELECT ind_sources.id ind_src_id, ind_sources.survey_id,`ind_data_source_survey`.name agency FROM ind_sources 
-LEFT JOIN `ind_data_source_survey` ON `ind_data_source_survey`.id = ind_sources.survey_id 
-WHERE ind_sources.ministry_id = 0 AND ind_sources.ministry_division_id = 0 AND ind_sources.office_agency_id = 0
-)tbl_agency
-LEFT JOIN ind_def_sources ids ON ids.source_id = tbl_agency.ind_src_id)temp0
-
-
-SELECT temp1.*,sid.indicator_number FROM(SELECT sg.title,ids.ind_id ,si.id sdg_indicators_id,si.goal_id,
-sdg_td.target_number,si.target_id,si.data_status,si.due_data_years FROM sdg_indicators si 
-LEFT JOIN ind_def_sources ids ON si.id = ids.ind_id
-LEFT JOIN sdg_goals sg ON sg.id = si.goal_id
-LEFT JOIN sdg_target_details sdg_td ON sdg_td.target_id = si.target_id AND sdg_td.language_id = 1
-WHERE ids.ind_id IS NOT NULL AND si.parent_indicator_id = 0 AND si.is_child = 0 
-AND si.is_npt_thirty_nine = 0 AND si.is_sdg = 1  AND si.is_plus_one = 0)temp1
-LEFT JOIN sdg_indicator_details sid ON sid.indicator_id = temp1.ind_id AND sid.language_id = 1
+#############
+SELECT temp2.* FROM(SELECT si.id sdg_indicators_idx,si.has_child,CASE WHEN si.has_child = 0  THEN MAX(ind.data_period) 
+WHEN si.has_child = 1 THEN (SELECT MAX(data_period) FROM indicator_data WHERE ind_id IN 
+(SELECT GROUP_CONCAT(parent.id)  parent_id FROM(SELECT id FROM sdg_indicators WHERE has_child = 1)child
+LEFT JOIN (SELECT id,parent_indicator_id FROM sdg_indicators)parent
+ON child.id = parent.parent_indicator_id
+ WHERE parent_indicator_id = si.id))
+END AS last_reporting_year FROM sdg_indicators si
+LEFT JOIN indicator_data ind ON ind.ind_id = si.id
+GROUP BY si.id)temp2 
