8c11ee11fab540a258b5c9e2f46121d086d07aa4
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-08-18 23:57:36 +0600

    data status update report

--- solution/sdg/summary_query/rough/data_status_update.sql
+++ solution/sdg/summary_query/rough/data_status_update.sql
@@ -1,10 +1,28 @@
-SELECT temp.ind_idx,temp.source_idx,CASE WHEN temp.next_report_year IS NULL THEN GROUP_CONCAT(DISTINCT temp.data_period) END AS due_data_year
- FROM(SELECT tempx.*,tempy.* 
-FROM(SELECT ind_data.ind_id ind_idx,ind_data.source_id source_idx,ind_data.data_period 
-FROM indicator_data ind_data
-)tempx
-LEFT JOIN
+SELECT tempx.*,tempy.* FROM
 (SELECT ind_id,source_id,next_report_year FROM data_update_status 
-WHERE  next_report_year <=YEAR(NOW()))tempy
-ON tempx.data_period = tempy.next_report_year AND tempx.ind_idx = tempy.ind_id AND tempx.source_idx = tempy.source_id)temp
-GROUP BY temp.ind_idx,source_idx;
+WHERE  next_report_year <=YEAR(NOW()) ORDER BY ind_id,source_id)tempx
+LEFT JOIN
+(SELECT ind_data.ind_id ind_idx,ind_data.source_id source_idx,ind_data.data_period 
+FROM indicator_data ind_data ORDER BY ind_idx,source_idx)tempy ON tempx.ind_id = tempy.ind_idx 
+AND tempx.source_id = tempy.source_idx AND tempx.next_report_year = tempy.data_period;
+
+############## joining query ##################
+SELECT temp2.ind_id,temp2.source_id,temp2.data_status,
+CASE WHEN temp2.data_status = "not updated" THEN temp2.missing_data_year_list END AS due_data_years
+FROM(SELECT temp1.ind_id,temp1.source_id,
+CASE WHEN temp1.total_next_report_year = temp1.missing_data_year THEN "updated"
+WHEN temp1.total_next_report_year > temp1.missing_data_year AND temp1.missing_data_year >0 THEN "not updated"
+WHEN temp1.missing_data_year = 0 THEN "has no base line"
+END AS data_status,temp1.missing_data_year_list
+FROM(SELECT temp0.ind_id,temp0.source_id,COUNT(temp0.next_report_year) total_next_report_year,
+SUM(CASE WHEN temp0.data_period IS NULL THEN 1 ELSE 0 END) AS missing_data_year,
+GROUP_CONCAT(DISTINCT CASE WHEN temp0.data_period IS NULL THEN temp0.next_report_year END )AS missing_data_year_list
+FROM(SELECT tempx.*,tempy.* FROM
+(SELECT ind_id,source_id,next_report_year FROM data_update_status 
+WHERE  next_report_year <=YEAR(NOW()) ORDER BY ind_id,source_id)tempx
+LEFT JOIN
+(SELECT ind_data.ind_id ind_idx,ind_data.source_id source_idx,ind_data.data_period 
+FROM indicator_data ind_data ORDER BY ind_idx,source_idx)tempy ON tempx.ind_id = tempy.ind_idx 
+AND tempx.source_id = tempy.source_idx AND tempx.next_report_year = tempy.data_period)temp0
+GROUP BY temp0.ind_id,temp0.source_id)temp1)temp2;
+
