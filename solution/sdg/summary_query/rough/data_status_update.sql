9039354ff5a1b0dca56a89782871f697a2b516cd
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-08-19 16:22:10 +0600

    data_status_report updated

--- solution/sdg/summary_query/rough/data_status_update.sql
+++ solution/sdg/summary_query/rough/data_status_update.sql
@@ -3,7 +3,11 @@ WHEN temp1.total_next_report_year <> temp1.missing_data_year  THEN "not updated"
 WHEN temp1.total_next_report_year = temp1.missing_data_year  THEN "has no base line"
 END AS data_status,temp1.missing_data_year_list due_data
 FROM (SELECT temp0.ind_id,temp0.source_id,MAX(temp0.next_report_year)curr_report_year,MAX(temp0.data_period) max_data_period,
-GROUP_CONCAT(DISTINCT CASE WHEN temp0.data_period IS NULL THEN temp0.next_report_year END )AS missing_data_year_list,
+GROUP_CONCAT(DISTINCT CASE WHEN temp0.data_period IS NULL AND temp0.next_report_year <> YEAR(NOW()) THEN temp0.next_report_year
+WHEN temp0.data_period IS NULL AND temp0.next_report_year = YEAR(NOW()) 
+THEN (SELECT CASE WHEN NOW() > CONCAT(YEAR(NOW())+1,"-",DATE_FORMAT(ind_def.last_entry_date,"%m-%d")) THEN YEAR(NOW()) END AS  last_entry_date 
+FROM ind_definitions ind_def WHERE ind_def.ind_id = temp0.ind_id)  
+END )AS missing_data_year_list,
 COUNT(temp0.next_report_year) total_next_report_year,
 SUM(CASE WHEN temp0.data_period IS NULL THEN 1 ELSE 0 END) AS missing_data_year
 FROM(SELECT tempx.*,tempy.* FROM
