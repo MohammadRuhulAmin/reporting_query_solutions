08c9c0fb345737e32f525f71c1dafda0dc0aa69b
Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-09-01 22:57:11 +0600

    custom query added

--- solution/research/transaction.sql
+++ solution/research/transaction.sql
@@ -7,3 +7,13 @@ DELETE FROM a WHERE sname = "ruhul amin";
 SET autocommit = 1; # to set commit to 1 for activating the auto commit mode 
 COMMIT;
 SELECT @@autocommit;
+
+# custom query
+
+SELECT "set autocommit = 0;" AS delete_statement
+UNION
+SELECT CONCAT("DELETE FROM ", TABLE_NAME, " WHERE application_id = 123;") AS delete_statement
+FROM information_schema.tables 
+WHERE table_schema = 'sdg-prod'
+UNION 
+SELECT "set autocommit = 1;" AS delete_statement;
