8c8bfc60ef6e6e8a1a43086de45a97059ffaa2b1
Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-09-01 21:27:33 +0600

    transaction in mysql

--- solution/research/database_settings.sql
+++ solution/research/database_settings.sql
@@ -2,3 +2,8 @@ SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
 
 SET sql_mode = '';
 SET GLOBAL group_concat_max_len = 9999999;
+
+# to check auto commit status:
+SELECT @@autocommit;
+# SET AUTO COMMIT STATUS TO ZERO:
+SET autocommit = 0;
