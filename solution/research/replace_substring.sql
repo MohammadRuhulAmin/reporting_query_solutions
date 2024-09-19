46d55f295ab441cbfe8e7a9279cdf5bbea285048
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-09-19 22:47:44 +0600

    REPLACE() function in MySQL

---
+++ solution/research/replace_substring.sql
@@ -0,0 +1,9 @@
+UPDATE hmis_pilgrim_img_archive
+SET details = REPLACE(details, 'old_sub_string', 'new_sub_string')
+WHERE id = 2;
+
+Example:
+
+UPDATE hmis_pilgrim_img_archive
+SET details = REPLACE(details, 'prp-img-prod', 'hmis-image-uat')
+WHERE id = 2;
