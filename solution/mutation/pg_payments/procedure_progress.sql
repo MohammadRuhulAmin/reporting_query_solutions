84a5fd4df7492841a695944b29daa341e9ca6016
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-10-24 09:44:53 +0600

    procedure step by step development

---
+++ solution/mutation/pg_payments/procedure_progress.sql
@@ -0,0 +1,47 @@
+DELIMITER //
+
+CREATE PROCEDURE challan_data_migration_procedure()
+BEGIN
+    DECLARE s_challan_id BIGINT;
+    DECLARE s_division_id BIGINT;
+    DECLARE s_application_id BIGINT;
+    DECLARE s_payment_by VARCHAR(255);
+    DECLARE s_is_wallet VARCHAR(3);
+    DECLARE s_client_unique_id VARCHAR(255);
+    DECLARE s_trns_req_id VARCHAR(255);
+    DECLARE s_payment_id BIGINT;
+    DECLARE s_tkm_id BIGINT;
+
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+        SELECT ("Error occured");
+    END;
+    SET autocommit = 0;
+    START TRANSACTION;
+
+    SELECT c.id, 
+           c.division_id,
+           SUBSTRING_INDEX(SUBSTRING_INDEX(c.client_unique_id, '#', 2), '#', -1) AS application_id,
+           c.payment_by,
+           IF(INSTR(c.payment_by, 'wallet') > 0, 'Yes', 'No') AS is_wallet,
+           c.client_unique_id, 
+           c.trns_req_id
+    INTO s_challan_id, s_division_id, s_application_id, s_payment_by, s_is_wallet, s_client_unique_id, s_trns_req_id
+    FROM pg_service.challan c
+    WHERE c.id > (SELECT IFNULL(MAX(id), 0) FROM pg_service_rnd.challan)
+      AND c.challan_generation_status = 1 
+      AND c.transaction_date < CURDATE()
+    ORDER BY c.id 
+    LIMIT 1;
+
+    INSERT INTO pg_service_rnd.challan SELECT * FROM pg_service.challan WHERE id = s_challan_id;
+    INSERT INTO pg_service_rnd.challan_history SELECT * FROM pg_service.challan_history WHERE challan_id = s_challan_id;
+    INSERT INTO pg_service_rnd.challan_log SELECT * FROM pg_service.challan_log WHERE challan_id = s_challan_id;
+    INSERT INTO pg_service_rnd.pg_payments SELECT * FROM pg_service.pg_payments WHERE client_unique_id = CONCAT('055#',s_client_unique_id);
+    
+    COMMIT;
+    SET autocommit = 1;
+END //
+
+DELIMITER ;
