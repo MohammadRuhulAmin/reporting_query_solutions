e65ae192de85247a0009bc9221a7c6ce5b1e286e
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-10-25 12:05:50 +0600

    max group concat length updated

--- solution/mutation/pg_payments/procedure_progress.sql
+++ solution/mutation/pg_payments/procedure_progress.sql
@@ -1,28 +1,31 @@
+
+
 DELIMITER //
 
 CREATE PROCEDURE challan_data_migration_procedure()
 BEGIN
-    DECLARE s_challan_id BIGINT;
-    DECLARE s_division_id BIGINT;
-    DECLARE s_application_id BIGINT;
+    DECLARE s_challan_id INT(11);
+    DECLARE s_division_id INT(11);
+    DECLARE s_application_id INT(11);
     DECLARE s_payment_by VARCHAR(255);
     DECLARE s_is_wallet VARCHAR(3);
     DECLARE s_client_unique_id VARCHAR(255);
     DECLARE s_trns_req_id VARCHAR(255);
-    DECLARE s_payment_id BIGINT;
+    DECLARE s_payment_id INT(11);
     DECLARE s_tkm_id_list VARCHAR(1000);
-    DECLARE s_tkmh_count BIGINT;
+    DECLARE s_tkmh_count INT(11);
     DECLARE error_message TEXT;
 
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
         GET DIAGNOSTICS CONDITION 1 error_message = MESSAGE_TEXT;
         ROLLBACK;
-        INSERT INTO `pg_service_rnd`.`challan_unsuccess_log`(challan_id, application_id, payment_by, is_wallet, client_unique_id, trans_req_id,error_message,`status`)
-	VALUES (s_challan_id,s_application_id,s_payment_by,s_is_wallet,s_client_unique_id,s_trns_req_id,error_message,-1);
+        INSERT INTO `pg_service_rnd`.`challan_unsuccess_log`(challan_id,division_id, application_id, payment_by, is_wallet, client_unique_id, trans_req_id,error_message,`status`)
+	VALUES (s_challan_id,s_division_id,s_application_id,s_payment_by,s_is_wallet,s_client_unique_id,s_trns_req_id,error_message,-1);
 	SELECT error_message, CONCAT(" challan_id : ",s_challan_id) challan_info;
 
     END;
+    SET SESSION group_concat_max_len = 1000000;
     SET autocommit = 0;
     START TRANSACTION;
 
@@ -44,9 +47,10 @@ BEGIN
     INSERT INTO pg_service_rnd.challan SELECT * FROM pg_service.challan WHERE id = s_challan_id;
     INSERT INTO pg_service_rnd.challan_history SELECT * FROM pg_service.challan_history WHERE challan_id = s_challan_id;
     INSERT INTO pg_service_rnd.challan_log SELECT * FROM pg_service.challan_log WHERE challan_id = s_challan_id;
-    INSERT INTO pg_service_rnd.pg_payments SELECT * FROM pg_service.pg_payments WHERE client_unique_id = CONCAT('055#',s_client_unique_id);
+    
     
     IF s_is_wallet ='No' THEN
+        INSERT INTO pg_service_rnd.pg_payments SELECT * FROM pg_service.pg_payments WHERE client_unique_id = CONCAT('055#',s_client_unique_id) AND transaction_id = s_trns_req_id;
         SELECT p.id INTO s_payment_id FROM pg_service.pg_payments p WHERE p.transaction_id = s_trns_req_id AND p.client_unique_id = CONCAT('055#', s_client_unique_id);
         INSERT INTO pg_service_rnd.pg_payment_log SELECT * FROM pg_service.pg_payment_log WHERE payment_id = s_payment_id;
 	INSERT INTO pg_service_rnd.pg_payments_history SELECT * FROM pg_service.pg_payments_history WHERE pg_payments_id = s_payment_id;
