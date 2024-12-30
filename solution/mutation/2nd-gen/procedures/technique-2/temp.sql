bade502cf3497de635bb44c68f4eb0fa70659177
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-12-30 14:54:22 +0600

    3.a and 3.b done

--- solution/mutation/2nd-gen/procedures/technique-2/temp.sql
+++ solution/mutation/2nd-gen/procedures/technique-2/temp.sql
@@ -6,10 +6,12 @@ BEGIN
     DECLARE p_application_id INT;
     DECLARE p_office_id INT;
     DECLARE p_status INT;
+    DECLARE p_union_office_name VARCHAR(255) DEFAULT "";
     #DECLARE res text default "";
     DECLARE error_message TEXT;
     DECLARE array_length INT;
     DECLARE counter1 INT DEFAULT 0;
+    DECLARE nth_working_date DATE;
     DECLARE is_only_kanungo TINYINT DEFAULT 0;
     DECLARE is_both_kanungo_surveyor TINYINT DEFAULT 0;
     DECLARE ks_id INT DEFAULT 0;
@@ -38,7 +40,7 @@ BEGIN
     ON DUPLICATE KEY UPDATE `status` = VALUES(`status`);
     
      
-    #step - 3.a 
+    #step - 3.a,3.b 
     SELECT JSON_ARRAYAGG(soap.application_id) AS process_application_list INTO applications_application_id_list
     FROM second_order_automation_process soap WHERE soap.status = 1;
     SET array_length = JSON_LENGTH(applications_application_id_list);
@@ -71,14 +73,57 @@ BEGIN
 	(SELECT id FROM case_status_updates WHERE application_id = p_application_id DESC LIMIT 1),
 	2,
 	DATE_FORMAT(CURDATE(), '%Y-%m-%d'),
-	/*for template: SELECT union_office_name FROM applications a LEFT JOIN 
-	office_wise_mouja_assign owma ON owma.mouja_id = a.mouja_id WHERE a.id = 3261844;
-	*/
-	(SELECT `template` FROM `text_templates` WHERE id = (SELECT text_template_id FROM `case_statuses` WHERE id = ks_id)),
+	
+	/*Get Nth working date*/	
+        SELECT tempx.today_date INTO nth_working_date FROM (
+	SELECT temp.today_date, ROW_NUMBER() OVER () AS working_day_sl 
+	FROM (
+	WITH RECURSIVE next_days AS (
+	    SELECT CURDATE() AS today_date, 0 AS day_offset
+	    UNION ALL
+	    SELECT DATE_ADD(today_date, INTERVAL 1 DAY), day_offset + 1
+	    FROM next_days
+	    WHERE day_offset < 29
+	)
+	SELECT today_date 
+	FROM next_days)temp 
+	WHERE 
+	temp.today_date NOT IN (SELECT holiday_date FROM govt_holiday WHERE is_active = 1))tempx
+	WHERE tempx.working_day_sl = 5;
+	
+	SELECT union_office_name INTO p_union_office_name FROM applications a LEFT JOIN 
+        office_wise_mouja_assign owma ON owma.mouja_id = a.mouja_id WHERE a.id = p_application_id;
+	
+	
+	(SELECT REPLACE(REPLACE(template,'{union}',p_union_office_name),'{next_status_date}',nth_working_date) FROM `text_templates` WHERE id = (SELECT text_template_id FROM `case_statuses` WHERE id = ks_id)),
 	1,
 	( SELECT signature FROM rsk.users WHERE office_id = (SELECT office_id FROM applications WHERE id = p_application_id) AND user_group_id = 4 AND idp_uuid IS NOT NULL),
 	NOW(),
-	()/*user info*/,
+	(SELECT 
+  CONCAT(
+    "{",
+    '"user_name_bn":"',  IFNULL(u.name_bn, ""), '",',
+    '"designation_bn":"',IFNULL(jd.name_bn, ""), '",',
+    '"office_name_bn":"',IFNULL((SELECT title_bn FROM rsk.jomi_offices WHERE id = (SELECT office_id FROM mutation_dhaka.applications WHERE id = 2833)),""), '"',
+    '"district_name_bn":"',IFNULL(
+    (SELECT jd.name_bn FROM rsk.jomi_offices jo
+     LEFT JOIN rsk.jomi_districts jd ON jd.division_id = jo.division_id AND jd.id = jo.district_id
+     WHERE jo.id = (SELECT office_id FROM mutation_dhaka.applications  WHERE id = 2833)
+     ),""),'",',
+    '"upazila_name_bn":"',IFNULL((SELECT ju.name_bd FROM rsk.jomi_offices jo
+	LEFT JOIN rsk.jomi_upazilas ju ON ju.division_id = jo.division_id 
+	AND ju.division_id = jo.division_id AND ju.district_id = jo.district_id 
+	AND ju.upazila_id = jo.upazila_id 
+	WHERE jo.id = (SELECT office_id FROM mutation_dhaka.applications  WHERE id = 11044388)),""),'",',
+	    "}"
+	  ) AS user_info_bn
+	FROM rsk.users u
+	LEFT JOIN rsk.jomi_designations jd 
+	  ON jd.id = u.designation_id
+	WHERE u.user_group_id = 4 
+	  AND u.office_id = 5 
+	  AND u.idp_uuid IS NOT NULL 
+	  AND u.status = 1)/*user info*/,
 	1
 	
       )
@@ -98,3 +143,9 @@ DELIMITER ;
 DROP PROCEDURE second_order_auto_placement_test;
 
 CALL second_order_auto_placement_test();
+
+/*
+To make bangla date: 
+SELECT REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:%s'), '0', '০'), '1', '১'), '2', '২'), '3', '৩'), '4', '৪'), '5', '৫'), '6', '৬'), '7', '৭'), '8', '৮'), '9', '৯') AS bangla_timestamp;
+
+*/
